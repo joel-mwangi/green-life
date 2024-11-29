eec528cad973962c39c9bab6428437efa2b04815
joel <162045633+joel-mwangi@users.noreply.github.com>
2024-11-29 12:13:03 +0300

    Update urls.py

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -24,7 +24,4 @@ urlpatterns = [
     path('', include('app_name.urls')),  # Replace 'app_name' with the actual name of your app
 ]
 if settings.DEBUG:
-    urlpatterns +=[ 
-                   static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-                   static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    ]
+    urlpatterns+=static(setting.MEDIA_URL,document_root=setting.MEDIA_ROOT)

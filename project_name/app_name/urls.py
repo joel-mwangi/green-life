cfd36ab1f2fc2a1b06aee894ed527600dc4c835e
joel <gachihijoel234@gmail.com>
2024-11-30 11:36:09 +0300

    Updates project_name/project_name/urls.py and  project_name/app_name/urls.py here

--- project_name/app_name/urls.py
+++ project_name/app_name/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+app_name = 'app_name'
 urlpatterns = [
     # Home page
     path('', views.home, name='home'),
--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_name.urls')),  # Replace 'app_name' with the actual name of your app
+    path('app_name/', include('app_name.urls')),  # Replace 'app_name' with the actual name of your app
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
